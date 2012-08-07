577d1d17b2a1710ccdbe81030b54acbfa8666a7c
illiatdesdindes <illiatdesdindes@gmail.com>
2012-08-07 18:04:45 +0200

    initial commit with test environement set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hallakl::Application.config.secret_token = '20aa9a451ba4679ffb194916cabae13a2f210eaefb4c1c835a495bfb66a0a757d377ab98408165a85eebaddd46596493ebecc69e6e38eec37b60a6b5ab67a779'
