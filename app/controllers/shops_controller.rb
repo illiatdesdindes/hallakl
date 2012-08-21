ba7f82cafc1ff2384d83d7c2d13712c37799162c
illiatdesdindes <illiatdesdindes@gmail.com>
2012-08-21 19:20:05 +0200

    finish create and update shops

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,32 @@ class ShopsController < ApplicationController
   def show
     @shop = Shop.find(params[:id])
   end
+  
+  def new
+    @shop = Shop.new
+  end
+  
+  def create
+    @shop = current_user.shops.build(params[:shop])
+    if @shop.save
+      flash[:success] = "Votre Etablissement est maintentant en ligne"
+      redirect_to @shop
+    else
+      render 'new'
+    end
+  end
+  
+  def edit 
+    @shop = Shop.find(params[:id])
+  end
+  
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update_attributes(params[:shop])
+      flash[:success] = "Modifications enregistrees"
+      redirect_to @shop
+    else
+      render 'edit'
+    end
+  end
 end
